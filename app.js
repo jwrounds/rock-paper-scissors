70271f233ac6336b6c8505b4d607fcc68acf0066
Jason Rounds <jwrounds@usfca.edu>
2020-09-01 13:36:02 -0500

    Refactored to add a game function with option of multiple game rounds

--- app.js
+++ app.js
@@ -6,17 +6,25 @@
 // either player or computer will be declared winner
 // win or lose message will alert the player to the computer's move and game result
 
-let playerMove;
-let validChoice = false;
 
-while (!validChoice) {
-    playerMove = prompt('Please pick and type either rock, paper, or scissors, or hit \'esc\' to cancel.');
-    if (playerMove === null) {
-        validChoice = true;
-        alert('Game cancelled');
-    } else if (playerMove.toLowerCase() === 'rock' || playerMove.toLowerCase() === 'paper' || playerMove.toLowerCase() === 'scissors') {
-        validChoice = true;
-        gameRound(playerMove, computerPlay());
+function game (rounds) {
+    +rounds;
+
+    for (let i = 0; i < rounds; i++) {
+    
+        let playerMove;
+        let validChoice = false;
+
+        while (!validChoice) {
+            playerMove = prompt('Please pick and type either rock, paper, or scissors, or hit \'esc\' to cancel.');
+            if (playerMove === null) {
+                validChoice = true;
+                alert('Game cancelled');
+            } else if (playerMove.toLowerCase() === 'rock' || playerMove.toLowerCase() === 'paper' || playerMove.toLowerCase() === 'scissors') {
+                validChoice = true;
+                gameRound(playerMove, computerPlay());
+            }
+        }
     }
 }
 
@@ -51,4 +59,11 @@ function gameRound (playerSelection, computerSelection) {
      } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
      }
+}
+
+let gameNumber = prompt('How many rounds would you like to play?');
+if (gameNumber === null || isNaN(gameNumber)) {
+    alert('Game cancelled');
+} else {
+    game(gameNumber);
 }
