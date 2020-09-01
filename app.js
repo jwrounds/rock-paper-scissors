e215b1687429fca6808c18d45576ef553071a6b7
Jason Rounds <jwrounds@usfca.edu>
2020-09-01 12:49:13 -0500

    Added game logic to encompass possible outcomes and determine winner

--- app.js
+++ app.js
@@ -31,3 +31,30 @@ function computerPlay () {
     return computerMove;
 }
 
+function gameRound (playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    console.log(playerSelection, computerSelection);
+     if (playerSelection === computerSelection) {
+         return alert(`${playerSelection} ties ${computerSelection}! No one loses! No one wins!`);
+
+     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+         return alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+
+     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
+
+     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+
+     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+         return alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
+
+     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+
+     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
+     }
+}
+
+gameRound(playerMove, computerPlay());
